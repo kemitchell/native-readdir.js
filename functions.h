f4cac514b9bb2a15974d16a6d571f890696f87ad
Kyle E. Mitchell <kyle@kemitchell.com>
2016-09-14 22:35:27 -0700

    Add code comments to header file

--- functions.h
+++ functions.h
@@ -5,6 +5,8 @@
 #include <dirent.h>
 #include <sys/types.h>
 
+// Wrapped constructible object that encapsulates a `DIR` pointer
+// used to call `readdir(3)`.
 class Directory : public Nan::ObjectWrap {
 public:
     static NAN_MODULE_INIT(Init);
@@ -14,12 +16,24 @@ private:
     ~Directory();
 
     static NAN_METHOD(New);
-    static NAN_METHOD(Open);
-    static NAN_METHOD(Read);
-    static NAN_METHOD(Close);
     static Nan::Persistent<v8::Function> constructor;
 
+    // Wraps `opendir(3)`
+    static NAN_METHOD(Open);
+
+    // Wraps `readdir(3)`
+    static NAN_METHOD(Read);
+
+    // Wraps `closedir(3)`
+    static NAN_METHOD(Close);
+
+    // C-string for the directory path. This is the argument to
+    // `opendir(3)`. The object keeps it around in case it needs to
+    // throw an error using NAN's `ErrnoException` helper.
     char* path;
+
+    // The return value of `opendir(3)`, which is passed repeatedly to
+    // `readdir(3)` and eventually to `closedir(3)`.
     DIR* directory;
 };
 
